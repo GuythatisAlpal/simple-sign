28ea7e8ab09e17a3600686738152fc09343006d1
Alex Quinones (Alpal) <alpalgamer2008@gmail.com>
2025-02-22 10:18:26 -0600

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background('black')
+  fill('cyan')
+  stroke ('white')
+  strokeWeight (10)
+  circle (250, 250, 400)
+  strokeWeight (0)
+  fill ('magenta')
+  textSize (40)
+  textStyle (BOLD)
+  text('Dont Just Look',100, 215)
+  text('Listen!',100, 285)
+  text('Be Aware', 100, 360)
+  rect(280, 260, 75, 75)
+  strokeWeight(0)
+   fill('sienna')
+  rect(300, 310, 35, 25)
+  fill('black')
+  rect(280,250, 75,15)
+  rect(305 ,320, 25,10)
+  fill('white')
+  stroke('black')
+  strokeWeight(3)
+  rect(295, 280, 15, 15)
+  rect(325, 280, 15, 15)
+  
+  
+  
+  
+  
+  
+
+  
+}
+
